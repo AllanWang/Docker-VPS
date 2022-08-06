d4d3117cb603a0a7164952fe9853e44e65e51896
Allan Wang <me@allanwang.ca>
2022-08-05 20:42:39 -0700

    Add new dst server

---
+++ dst/xd/configs/dedicated_server_mods_setup.lua
@@ -0,0 +1,28 @@
+-- Wormhole Marks
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=362175979
+ServerModSetup("362175979")
+-- Global Position
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=378160973
+ServerModSetup("378160973")
+-- Finder Mod
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=786654500
+ServerModSetup("786654500")
+-- DST Equipment Switcher
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=632087118
+ServerModSetup("632087118")
+-- Simple Health Bar DST
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=1207269058
+ServerModSetup("1207269058")
+-- Campfire Respawn
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=569043634
+ServerModSetup("569043634")
+-- Display Attack Range
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=2078243581
+ServerModSetup("2078243581")
+-- Increased Stack Size
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=374550642
+ServerModSetup("374550642")
+-- No Thermal Stone Durability
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=466732225
+ServerModSetup("466732225")
+
---
+++ dst/xd/configs/modoverrides.lua
@@ -0,0 +1,29 @@
+return {
+  -- Wormhole Marks
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=362175979
+  ["workshop-362175979"] = {enabled = true},
+  -- Global Position
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=378160973
+  ["workshop-378160973"] = {enabled = true},
+  -- Finder Mod
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=786654500
+  ["workshop-786654500"] = {enabled = true},
+  -- DST Equipment Switcher
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=632087118
+  ["workshop-632087118"] = {enabled = true},
+  -- Simple Health Bar DST
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=1207269058
+  ["workshop-1207269058"] = {enabled = true},
+  -- Campfire Respawn
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=569043634
+  ["workshop-569043634"] = {enabled = true},
+  -- Display Attack Range
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=2078243581
+  ["workshop-2078243581"] = {enabled = true},
+  -- Increased Stack Size
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=374550642
+  ["workshop-374550642"] = {enabled = true},
+  -- No Thermal Stone Durability
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=466732225
+  ["workshop-466732225"] = {enabled = true}
+}
