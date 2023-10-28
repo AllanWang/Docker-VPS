82e3e0de40c74a22863ae15797b387f0be22f832
Allan Wang <me@allanwang.ca>
2023-10-27 19:54:13 -0700

    Add new dst world

---
+++ dst/double-u/configs/dedicated_server_mods_setup.lua
@@ -0,0 +1,18 @@
+-- Wormhole Marks
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=362175979
+ServerModSetup("362175979")
+-- Global Position
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=378160973
+ServerModSetup("378160973")
+-- Finder Mod
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=786654500
+ServerModSetup("786654500")
+-- Simple Health Bar DST
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=1207269058
+ServerModSetup("1207269058")
+-- Campfire Respawn
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=569043634
+ServerModSetup("569043634")
+-- Show Bundle
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=1111658995
+ServerModSetup("1111658995")
---
+++ dst/double-u/configs/modoverrides.lua
@@ -0,0 +1,20 @@
+return {
+  -- Wormhole Marks
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=362175979
+  ["workshop-362175979"] = {enabled = true},
+  -- Global Position
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=378160973
+  ["workshop-378160973"] = {enabled = true},
+  -- Finder Mod
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=786654500
+  ["workshop-786654500"] = {enabled = true},
+  -- Simple Health Bar DST
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=1207269058
+  ["workshop-1207269058"] = {enabled = true},
+  -- Campfire Respawn
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=569043634
+  ["workshop-569043634"] = {enabled = true},
+  -- Show Bundle
+  -- https://steamcommunity.com/sharedfiles/filedetails/?id=1111658995
+  ["workshop-1111658995"] = {enabled = true} -- Watch comma
+}
